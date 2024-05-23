c983c8aea3c2cf0dc4e0beaa5f57b0dbc201739f
Horus <dandresp21@gmail.com>
2024-05-23 09:30:04 -0500

    @dump

--- resources/views/departments/edit.blade.php
+++ resources/views/departments/edit.blade.php
@@ -13,7 +13,6 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <!-- Contenido de la CARD -->
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @dump($employee)
                     <form method="POST" action="{{ route('departments.update', $employee) }}" >
                         @method('PUT')
                         @csrf
